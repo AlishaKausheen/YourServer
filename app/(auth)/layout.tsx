20ae12d835087025b27b64c97cac9cd461b2795e
AlishaKausheen <alishakausheen@gmail.com>
2023-11-16 23:15:48 -0800

    authenticatiob signin and signout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 const AuthLayout = ({children}:{children:React.ReactNode}) => {
     return (
-        <div className="bg-red-500 h-full">
+        <div className="bg-blue-500 h-full">
             {children}
         </div>
     )
