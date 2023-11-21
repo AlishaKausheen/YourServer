6ae8c83f8c3734fb7efb3d4cc929317027435046
AlishaKausheen <alishakausheen@gmail.com>
2023-11-21 00:01:42 -0800

    prisma studio

--- lib/initial-profile.ts
+++ lib/initial-profile.ts
@@ -8,5 +8,21 @@ export const initialProfile =async () => {
     if (!user) {
         return redirectToSignIn();
     }
-    const profile = await db.profile;
+    const profile = await db.profile.findUnique({
+        where: {
+            userId: user.id
+        }
+    });
+    if (profile) {
+        return profile;
+     }
+    const newProfile = await db.profile.create({
+        data: {
+            userId: user.id,
+            name: `$(user.firstName) $(user.lastName)`,
+            imageUrl: user.imageUrl,
+            email: user.emailAddresses[0].emailAddress
+        }      
+    })
+    return newProfile;
 }
